af00a40caaf78cb374981f04aa4a9d5f67a7c4d4
Neha Malhotra <nehamalhotra8899@gmail.com>
2019-12-06 11:56:11 -0800

    add navagaition to shop.cshtml page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,6 +19,7 @@ namespace Amaya.Controllers
         {
             return View();
         }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
